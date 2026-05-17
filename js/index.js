5e0891489db3c65465071dd5c493771481179789
Christian Chourio <christianchouriox@hotmail.com>
2026-05-17 00:24:19 -0400

    js added, created footer and skills

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
-let button = document.querySelector('button')
+// Footer
+let date = new Date()
+let thisYear = date.getFullYear()
 
-button.addEventListener('click', () => {
-    alert('Clicked')
-})
+let footer = document.querySelector('footer')
+let copyRight = document.createElement('p')
+copyRight.innerHTML = `&copy; ${thisYear} Christian Chourio`
+footer.appendChild(copyRight)
 
-console.log(button);
+// Add Skills
+let skills = ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub', 'Responsive Design', 'DOM Manipulation', 'REST APIs']
+let skillsList = document.querySelector('#skills ul')
+
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement('li')
+    skill.innerHTML = skills[i]
+    skillsList.appendChild(skill)    
+}
